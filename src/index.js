73f2e5e54deb4f8a7c8fed636af05a5758575306
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-27 23:43:08 +0530

    learning reac

--- src/index.js
+++ src/index.js
@@ -14,4 +14,20 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const navbar = (
+  <nav>
+      <h1>Bob's Bistro</h1>
+      <ul>
+          <li>Menu</li>
+          <li>About</li>
+          <li>Contact</li>
+      </ul>
+  </nav>
+)
+
+// ReactDOM.render(navbar, document.getElementById("root"))
+ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+
+
+
 reportWebVitals();
