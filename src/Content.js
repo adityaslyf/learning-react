85f3eea8c2258984705567a2e7ca6dde86bf7cb2
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-29 03:24:53 +0530

    added buttons in content.js and delets some files

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,35 @@
-
+import { useState } from "react";
 
 
 const Content = () => {
+  const[name , setName] = useState('Aditya');
+const[count , setCount] = useState(0);
+
+
 const handleNameChange = () =>{
   const names = ['Aditya','Shubham'];
   const int = Math.floor(Math.random() * 2);
-  return names[int];
+  setName(names[int]);
+}
+const handleClick = () => {
+  console.log(0);
+}
+const handleClick2 = (name) => {
+  console.log('{name} you clicked it');
+}
+const handleClick3 = (e) => {
+  console.log(e.target.innerText);
 }
 
-  return (
+  return (  
     <main>
-      Hello {handleNameChange()}!
+      <p onDoubleClick={handleClick}>Hello {name}!</p>
+
+      <button onClick={handleNameChange}>Click it</button>
+      <button onClick={handleClick}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button>
     </main>
+    
   )
 }
 export default Content;
