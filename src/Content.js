691f725b89ad865dc21ecc23b08c6124018a8da6
Aditya Varshney <aditya.varshneymail@gmail.com>
2023-08-30 23:16:17 +0530

    commenting things

--- src/Content.js
+++ src/Content.js
@@ -2,32 +2,33 @@ import { useState } from "react";
 
 
 const Content = () => {
-  const[name , setName] = useState('Aditya');
-const[count , setCount] = useState(0);
+  const[items , setItems] = useState('Aditya');
+// const[count , setCount] = useState(0);
 
 
-const handleNameChange = () =>{
-  const names = ['Aditya','Shubham'];
-  const int = Math.floor(Math.random() * 2);
-  setName(names[int]);
-}
-const handleClick = () => {
-  console.log(0);
-}
-const handleClick2 = (name) => {
-  console.log('{name} you clicked it');
-}
-const handleClick3 = (e) => {
-  console.log(e.target.innerText);
-}
+// const handleNameChange = () =>{
+//   const names = ['Aditya','Shubham'];
+//   const int = Math.floor(Math.random() * 2);
+//   setName(names[int]);
+// }
+// const handleClick = () => {
+//   setCount(a => a+1);
+//   console.log(count);
+// }
+// const handleClick2 = (name) => {
+//   console.log('${name} you clicked it');
+// }
+// const handleClick3 = (e) => {
+//   console.log(e.target.innerText);
+// }
 
   return (  
     <main>
-      <p onDoubleClick={handleClick}>Hello {name}!</p>
+      {/* <p onDoubleClick={handleClick}>Hello {name}!</p>
 
       <button onClick={handleNameChange}>Click it</button>
       <button onClick={handleClick}>Click it</button>
-      <button onClick={(e) => handleClick3(e)}>Click it</button>
+      <button onClick={(e) => handleClick3(e)}>Click it</button> */}
     </main>
     
   )
--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const page = (
+    
+    <div>
+    <img src="./react-logo.png" width = "40px"></img>
+    <h1>FUN FACTS ABOUT REACT</h1>
+    <ul>
+    <li>123</li>
+    <li>345</li>
+    <li>526</li>
+    
+    
+    
+    </ul>
+    
+    
+    </div>
+    
+    
 );
 
-
-
+ReactDOM.render(
+    page, document.getElementById("root")
+)
